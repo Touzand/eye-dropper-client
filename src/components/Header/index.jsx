6f6c814df48417132af899cdd8bbfc583ae87cdb
Alan Alonso <alan.alonso.gaf15@gmail.com>
2023-01-28 20:01:49 -0300

    header/css updated

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,24 +1,26 @@
 import { Link } from "react-router-dom";
-import { HeaderContainer, LinkPage } from "./style";
+import { HeaderContainer, LeftHeaderSide,  RightHeaderSide } from "./style";
 
 const Header = () => {
   return (
     <HeaderContainer>
       <div className="sub-header">
-        <div className="right">
+        <RightHeaderSide>
           <img
             src="http://imgfz.com/i/OEygR5o.png"
             alt="Logo"
             className="top-header-logo"
           />
           Min eye dropper
-        </div>
-        <div className="left buttons">
+        </RightHeaderSide>
+        <LeftHeaderSide>
+          <div className='buttons'>
           <a href="/eye-dropper-client/">Home</a>
           <Link to="/eye-dropper-client/privacy-policy">Privacy Police</Link>
           <Link className="not-found" to="">Help</Link>
           <Link className="not-found" to="">Change logs</Link>
-        </div>
+          </div>
+        </LeftHeaderSide>
       </div>
     </HeaderContainer>
   );
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -13,39 +13,52 @@ export const HeaderContainer = styled.header`
   color: whitesmoke;
   background-color: var(--background-color-2);
 
-  .sub-header {
-    display: flex;
-    align-items: center;
-    justify-content:center.;
-    flex-direction:column;
-    gap:1rem;
-
-
-    .right {
-      display: flex;
-      align-items: center;
-      gap: 0.5rem;
-      font-size: 1.25rem;
-      font-weight: unset;
-    }
-
-    .left {
-      width:100vw;
-    }
-  }
-
   img {
     width: 40px;
     height: 100%;
   }
 
+  .sub-header {
+    display: flex;
+    align-items: center;
+    justify-content: center.;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  @media (min-width: 800px) {
+    .sub-header {
+      width: min(1000px, 100%);
+      flex-direction: row;
+      justify-content: space-between;
+
+      .buttons {
+        gap: 1rem;
+        padding-top: 0;
+        border-top: none;
+      }
+    }
+  }
+`;
+
+export const RightHeaderSide = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  font-size: 1.25rem;
+  font-weight: unset;
+`;
+
+export const LeftHeaderSide = styled.div`
+  width: 100vw;
+
   .buttons {
     display: flex;
-    align-items:center;
-    justify-content:space-around;
+    align-items: center;
+    justify-content: space-around;
     gap: 0.5rem;
-    border-top:solid thin #223157;
-    padding-top:1rem;
+    border-top: solid thin #223157;
+    padding-top: 1rem;
 
     .not-found {
       color: grey;
@@ -72,25 +85,6 @@ export const HeaderContainer = styled.header`
   }
 
   @media (min-width: 800px) {
-    .sub-header {
-      width: min(1000px,100%);
-      flex-direction:row;
-      justify-content:space-between;
-
-      .right {
-      }
-
-      .buttons{
-      gap:1rem;
-        padding-top:0;
-        border-top:none;
-    }
-
-      .left {
-        width:auto;
-      }
-    }
+    width: auto;
   }
 `;
-
-export const LinkPage = styled.a``;
